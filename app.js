7bcc622dd2df748dc213a59a4d77bf6f3cb8f714
Enzo B. <enzo.balsen@web.de>
2023-10-09 16:42:45 +0200

    videos

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var frameguard = require("frameguard");
+const helmet = require('helmet');
 
 
 var indexRouter = require('./routes/index');
@@ -24,8 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(frameguard({ action: "SAMEORIGIN" }));
-
+app.use(helmet.frameguard({action: "SAMEORIGIN"}));
 
 app.use('/users', usersRouter);
 app.use('/class', classRouter);
